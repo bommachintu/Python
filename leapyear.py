fa9caf7922bec36e131ffff8e549bf68e1bf3e0b
BommaRahul <bommachintu.com>
2020-03-05 13:50:26 -0500

    leap year program

---
+++ leapyear.py
@@ -0,0 +1,15 @@
+def is_leap(year):
+    if (year % 4) == 0:
+        if (year % 100) == 0:
+            if (year % 400) == 0:
+                print("True")
+            else:
+                print("False")
+        else:
+             print("True")
+    else:
+        return "False"
+
+
+year = int(input())
+print(is_leap(year))
