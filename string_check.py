01b22b94fd2798e6ea810526a5487fd2d8b9ebe9
BommaRahul <bommachintu.com>
2020-03-07 12:17:53 -0500

    string methods

---
+++ string_check.py
@@ -0,0 +1,32 @@
+st = "Qa1"
+for i in range(len(st)):
+    if(st[i].isalnum()):
+        print("True")
+        break;
+    elif(i==len(st)-1):
+        print("False")
+for i in range(len(st)):
+    if(st[i].isalpha()):
+        print("True")
+        break;
+    elif(i==len(st)-1):
+        print("False")
+for i in range(len(st)):
+    if(st[i].isdigit()):
+        print("True")
+        break;
+    elif(i==len(st)-1):
+        print("False")
+for i in range(len(st)):
+    if((st[i].islower())):
+        print("True")
+        break;
+    elif(i==len(st)-1):
+        print("False")
+for i in range(len(st)):
+    if((st[i].isupper())):
+        print("True")
+        break;
+    elif(i==len(st)-1):
+        print("False")
+        
